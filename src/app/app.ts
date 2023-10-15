a530e16c6cd6a65d9a062fe2ddab5543f13fadbd
Domino731 <d.orzechowski18@gmail.com>
2023-10-15 17:53:58 +0200

    feature - project setup

--- src/app/app.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { APP_PORT } from './app.config';
-
-export const app = async (): Promise<void> => {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(APP_PORT);
-};
